d6a873b0802023931474b5808ecb7c25f12e536a
Tonniekangethe <tonniekamau64@gmail.com>
2023-04-26 15:30:49 -0700

    decimal

---
+++ _dec.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *print_dec - print an integer to base 10
+ * @args: variadic parameter
+ *
+ * Return: number of characters
+*/
+int print_dec(va_list args)
+{
+	int i = 0;
+	int count = 0;
+	int num;
+	int arr[10];
+	char x[1];
+
+	num = va_arg(args, int);
+
+	while (num != 0)
+	{
+		arr[i] = (num % 10);
+		num = num / 10;
+		if (num == 0)
+			break;
+		i++;
+	}
+
+	for (i ; i >= 0 ; 1--)
+	{
+		x[0] = ('0' + arr[i]);
+
+		count += write(1, x, 1);
+	}
+	return (count);
+}
