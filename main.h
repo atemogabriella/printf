9ded501bbe89d900f663f4c2aa1e012876a04fd2
atemogabriella <atemogabriella0@gmail.com>
2023-04-26 17:28:59 -0700

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- main.h
+++ main.h
@@ -3,23 +3,8 @@
 
 #include <unistd.h>
 #include <stdarg.h>
-<<<<<<< HEAD
 #include <unistd.h>
 int _printf(const char *format, ...);
-=======
-
-
-int _printf(const char *format, ...);
-int (*_specifier(const char *format))(va_list);
-/**
- * struct cnd - conditions to be printed
- * @condition: condition of the character to compare
- * @f: function that stores the string to be printed
- */ 
-typedef struct cnd
-{
-	char * condition;
-	int (*f) (va_list);
-}cnd_dt;
->>>>>>> 27f9d46118ab643558fc59e2888f8be986044f7b
+void print_string(va_list args, int *count);
+void print_char(va_list args, int *count);
 #endif
--- printf.c
+++ printf.c
@@ -1,121 +1,91 @@
 #include "main.h"
-<<<<<<< HEAD
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ * print_char - print a single character
+ * @args: a list of arguments
+ * @count: pointer to count of characters printed
+ * Return: void
+ */
+void print_char(va_list args, int *count)
+{
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+	(*count)++;
+}
+
+/**
+ * print_string - prints a string
+ * @args: a list of arguments
+ * @count: pointer to count of characters printed
+ * Return: void
+ */
+void print_string(va_list args, int *count)
+{
+	char *s = va_arg(args, char*);
+
+	if (s == NULL)
+	{
+		write(1, "(null)", 6);
+		(*count) += 6;
+	}
+	else
+	{
+		while (*s != '\0')
+		{
+			write(1, s, 1);
+			(*count)++;
+			s++;
+		}
+	}
+}
+
 /**
  * _printf - improvised printf function
  * @format: string format to be printed
  * Return: number of characters printed
-=======
-/**
- * _printf - printf function
- * @format: variadic parameter
- *
- * Return: count if successful
->>>>>>> 27f9d46118ab643558fc59e2888f8be986044f7b
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-	char c;
-	char *s;
 
 	va_start(args, format);
 
 	if (*format == '\0')
 		return (-1);
-
-<<<<<<< HEAD
 	while (*format != '\0')
 	{
 		if (*format == '%')
-=======
-	while (format && format[i] != '\0')
-	{
-		if (format[i] != '%')
-		{
-			number = write(1, &format[i], 1);
-			count = count + number;
-			i++;
-			continue;
-		}
-	
-		if (format[i] == '%')
->>>>>>> 27f9d46118ab643558fc59e2888f8be986044f7b
 		{
 			format++;
 			switch (*format)
 			{
-<<<<<<< HEAD
 				case 'c':
-				{
-					c = va_arg(args, int);
-
-					write(1, &c, 1);
-					count++;
+					print_char(args, &count);
 					break;
-				}
 				case 's':
-				{
-					s = va_arg(args, char*);
-
-					if  (s == NULL)
-					{
-						write(1, "(null)", 6);
-						count = count + 6;
-						break;
-					}
-					while (*s != '\0')
-					{
-						write(1, s, 1);
-						count++;
-						s++;
-					}
+					print_string(args, &count);
 					break;
-				}
 				case '%':
-				{
 					write(1, format, 1);
 					count++;
 					break;
-				}
 				default:
-				{
-				c = '%';
-				write(1, &c, 1);
-				write(1, format, 1);
-				count = count + 2;
-				break;
-				}
+					write(1, "%", 1);
+					write(1, format, 1);
+					count += 2;
+					break;
 			}
 		}
 		else
 		{
 			write(1, format, 1);
 			count++;
-=======
-				number = f(args);
-				count = count + number;
-				i = 1 + 2;
-			}
-		}
-		if (format[i + 1] == '\0')
-		{
-			break;
-		}
-		if (format[i + 1] != '\0')
-		{
-			number = write(1, &format[i + 1], 1);
-			count = count + number;
-			i = i + 2;
-			continue;
->>>>>>> 27f9d46118ab643558fc59e2888f8be986044f7b
 		}
 		format++;
 	}
 	va_end(args);
 	return (count);
-
 }
-
