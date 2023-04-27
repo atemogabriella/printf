15eb7acfd703dc4d8e184b3336d0a00018ffb7ac
atemogabriella <atemogabriella0@gmail.com>
2023-04-27 08:31:36 -0700

    printf

--- Task1.c
+++ Task1.c
@@ -31,3 +31,61 @@ void print_int(va_list args, int *count)
 	}
 }
 
+/**
+ * _printf - produces output according to format
+ * @format: character string of zero or more directives
+ * Return: number of characters printed except the null byte
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i, count = 0;
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+		switch (format[i])
+		{
+			case 'c':
+				putchar(va_arg(args, int));
+				count++;
+				break;
+			case 's':
+			{
+				char *str = va_arg(args, char *);
+				while (str != '\0')
+				{
+					putchar(*str);
+					str++;
+					count++;
+				}
+			}
+			break;
+
+		case '%':
+			putchar('%');
+			count ++;
+			break;
+
+		case 'd':
+		case 'i':
+			print_int(args, count);
+			break;
+
+		default:
+			putchar('%');
+			putchar(format[i]);
+			count += 2;
+			break;
+		}
+	}
+	else
+	{
+		putchar(format[i]);
+		count++;
+	}
+	va_end(args);
+	return (count);
+}
